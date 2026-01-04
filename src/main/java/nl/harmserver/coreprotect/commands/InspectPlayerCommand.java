81d7d66da5858d4acd125d9c443a9160bc756ec5
Matsoe1000 <mjtvandenbrink@gmail.com>
2026-01-04 10:51:09 +0100

    Update InspectPlayerCommand.java

--- src/main/java/nl/harmserver/coreprotect/commands/InspectPlayerCommand.java
+++ src/main/java/nl/harmserver/coreprotect/commands/InspectPlayerCommand.java
@@ -1,19 +1,52 @@
+package nl.harmserver.coreprotect.commands;
+
+import nl.harmserver.coreprotect.listeners.BlockLogger;
+import nl.harmserver.coreprotect.listeners.BlockLogger.LogEntry;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class InspectPlayerCommand implements CommandExecutor {
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (args.length < 1) {
             sender.sendMessage(ChatColor.RED + "Gebruik: /inspectplayer <speler>");
             return true;
         }
+
         String targetPlayer = args[0];
-        for (BlockLogger.LogEntry entry : BlockLogger.logs) {
+        boolean found = false;
+
+        for (LogEntry entry : BlockLogger.logs) {
             if (entry != null && entry.player.equalsIgnoreCase(targetPlayer)) {
-                sender.sendMessage(ChatColor.YELLOW + entry.player + ChatColor.GRAY +
-                        " " + entry.action + " " + entry.blockType + " @ " + entry.location +
-                        " (" + entry.time + ")");
+                found = true;
+                String timeFormatted = dateFormat.format(new Date(entry.time));
+                sender.sendMessage(ChatColor.YELLOW + "[InspectPlayer] " +
+                        ChatColor.AQUA + entry.player +
+                        ChatColor.GRAY + " heeft " +
+                        ChatColor.GOLD + entry.action +
+                        ChatColor.GRAY + " gedaan op " +
+                        ChatColor.GREEN + entry.blockType.name() +
+                        ChatColor.GRAY + " @ " +
+                        ChatColor.DARK_GREEN + "(" + entry.location.getBlockX() + "," +
+                        entry.location.getBlockY() + "," +
+                        entry.location.getBlockZ() + ")" +
+                        ChatColor.GRAY + " (" + timeFormatted + ")");
             }
         }
+
+        if (!found) {
+            sender.sendMessage(ChatColor.GRAY + "[InspectPlayer] Geen logs gevonden voor speler " +
+                    ChatColor.YELLOW + targetPlayer);
+        }
+
         return true;
     }
 }
-
