70e079848e219cc7361da15e8231f2f19c710b9b
Matsoe1000 <mjtvandenbrink@gmail.com>
2026-01-04 10:48:05 +0100

    Update BlockLogger.java

--- src/main/java/nl/harmserver/coreprotect/listeners/BlockLogger.java
+++ src/main/java/nl/harmserver/coreprotect/listeners/BlockLogger.java
@@ -1,9 +1,8 @@
 package nl.harmserver.coreprotect.listeners;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import nl.harmserver.coreprotect.HarmServerCoreProtect;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,39 +10,74 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlockLogger implements Listener {
 
-    public static class LogEntry {
-        public final String player;
-        public final String action;
-        public final String blockType;
-        public final Location location;
-        public final long time;
-
-        public LogEntry(String player, String action, String blockType, Location location, long time) {
-            this.player = player;
-            this.action = action;
-            this.blockType = blockType;
-            this.location = location;
-            this.time = time;
-        }
-    }
-
     public static final List<LogEntry> logs = new ArrayList<>();
+    private static File logFile;
 
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        Player player = event.getPlayer();
-        Block block = event.getBlock();
-        logs.add(new LogEntry(player.getName(), "BREAK", block.getType().toString(),
-                block.getLocation(), System.currentTimeMillis()));
+    public BlockLogger() {
+        // Maak logbestand aan in plugin folder
+        File dataFolder = HarmServerCoreProtect.getInstance().getDataFolder();
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
+        }
+        logFile = new File(dataFolder, "blocklogs.txt");
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         Player player = event.getPlayer();
         Block block = event.getBlockPlaced();
-        logs.add(new LogEntry(player.getName(), "PLACE", block.getType().toString(),
-                block.getLocation(), System.currentTimeMillis()));
+        LogEntry entry = new LogEntry(player.getName(), "PLACE", block.getType(), block.getLocation(), System.currentTimeMillis());
+        logs.add(entry);
+        writeToFile(entry);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+        LogEntry entry = new LogEntry(player.getName(), "BREAK", block.getType(), block.getLocation(), System.currentTimeMillis());
+        logs.add(entry);
+        writeToFile(entry);
+    }
+
+    private void writeToFile(LogEntry entry) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true))) {
+            writer.write(entry.toString());
+            writer.newLine();
+        } catch (IOException e) {
+            HarmServerCoreProtect.getInstance().getLogger().warning("Kon log niet wegschrijven: " + e.getMessage());
+        }
+    }
+
+    public static class LogEntry {
+        public final String player;
+        public final String action;
+        public final Material blockType;
+        public final Location location;
+        public final long time;
+
+        public LogEntry(String player, String action, Material blockType, Location location, long time) {
+            this.player = player;
+            this.action = action;
+            this.blockType = blockType;
+            this.location = location;
+            this.time = time;
+        }
+
+        @Override
+        public String toString() {
+            return player + ";" + action + ";" + blockType.name() + ";" +
+                    location.getWorld().getName() + "," + location.getBlockX() + "," + location.getBlockY() + "," + location.getBlockZ() +
+                    ";" + time;
+        }
     }
 }
