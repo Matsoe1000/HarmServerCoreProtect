b47fd9dee1ae7dd167dd4df5cfe63880372f98cc
Matsoe1000 <mjtvandenbrink@gmail.com>
2026-01-04 10:52:06 +0100

    Update ReloadCommand.java

--- src/main/java/nl/harmserver/coreprotect/commands/ReloadCommand.java
+++ src/main/java/nl/harmserver/coreprotect/commands/ReloadCommand.java
@@ -1,10 +1,23 @@
+package nl.harmserver.coreprotect.commands;
+
+import nl.harmserver.coreprotect.HarmServerCoreProtect;
+import nl.harmserver.coreprotect.listeners.BlockLogger;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
 public class ReloadCommand implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        // Hier kun je config opnieuw laden of logs resetten
+        // Config opnieuw laden
+        HarmServerCoreProtect.getInstance().reloadConfig();
+
+        // Logs wissen
         BlockLogger.logs.clear();
-        sender.sendMessage(ChatColor.GREEN + "CoreProtect plugin herladen. Logs zijn gewist.");
+
+        sender.sendMessage(ChatColor.GREEN + "[CoreProtectReload] Config herladen en logs gewist.");
         return true;
     }
 }
-
