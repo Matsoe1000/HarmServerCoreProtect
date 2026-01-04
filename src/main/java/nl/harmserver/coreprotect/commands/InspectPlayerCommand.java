6406d02183b7c7e0a6e156030f803eff0c1335cf
Matsoe1000 <mjtvandenbrink@gmail.com>
2026-01-04 10:45:41 +0100

    Update InspectPlayerCommand.java

--- src/main/java/nl/harmserver/coreprotect/commands/InspectPlayerCommand.java
+++ src/main/java/nl/harmserver/coreprotect/commands/InspectPlayerCommand.java
@@ -1,1 +1,19 @@
+public class InspectPlayerCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage(ChatColor.RED + "Gebruik: /inspectplayer <speler>");
+            return true;
+        }
+        String targetPlayer = args[0];
+        for (BlockLogger.LogEntry entry : BlockLogger.logs) {
+            if (entry != null && entry.player.equalsIgnoreCase(targetPlayer)) {
+                sender.sendMessage(ChatColor.YELLOW + entry.player + ChatColor.GRAY +
+                        " " + entry.action + " " + entry.blockType + " @ " + entry.location +
+                        " (" + entry.time + ")");
+            }
+        }
+        return true;
+    }
+}
 
