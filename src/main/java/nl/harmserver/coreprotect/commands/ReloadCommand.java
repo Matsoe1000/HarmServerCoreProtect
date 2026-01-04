9dbd4e01468a8964a9e23027bc1ad4bae8781ea6
Matsoe1000 <mjtvandenbrink@gmail.com>
2026-01-04 10:46:59 +0100

    Update ReloadCommand.java

--- src/main/java/nl/harmserver/coreprotect/commands/ReloadCommand.java
+++ src/main/java/nl/harmserver/coreprotect/commands/ReloadCommand.java
@@ -1,1 +1,10 @@
+public class ReloadCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        // Hier kun je config opnieuw laden of logs resetten
+        BlockLogger.logs.clear();
+        sender.sendMessage(ChatColor.GREEN + "CoreProtect plugin herladen. Logs zijn gewist.");
+        return true;
+    }
+}
 
